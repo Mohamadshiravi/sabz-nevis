63c225481db00f394fd726d3bbb5bbad10a9ca1f
mohamad <mohamadshiravi85@gmail.com>
2025-01-07 14:07:31 +0330

    fixed btn bug

--- components/module/primaryBtn.tsx
+++ components/module/primaryBtn.tsx
@@ -9,7 +9,7 @@ export default function PrimaryBtn({ children, onPress }: PrimaryBtnProps) {
   return (
     <button
       onClick={onPress ? onPress : undefined}
-      className="sm:text-sm text-xs vazir-medium hover:bg-zinc-800 hover:text-white transition px-4 py-1 border-2 border-zinc-800 dark:border-zinc-300 dark:text-zinc-300 dark:hover:bg-zinc-300 dark:hover:text-zinc-800 rounded-full"
+      className="text-sm vazir-medium hover:bg-zinc-800 hover:text-white transition px-4 py-1 border-2 border-zinc-800 dark:border-zinc-300 dark:text-zinc-300 dark:hover:bg-zinc-300 dark:hover:text-zinc-800 rounded-full"
     >
       {children}
     </button>
