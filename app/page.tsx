b8922438d497c3d4ed70bf9b83af845e428fc5e2
mohamad <mohamadshiravi85@gmail.com>
2025-01-11 14:42:20 +0330

    test loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import FirstLoading from "@/components/template/loading/firstLoading";
 import { redirect } from "next/navigation";
 
-export default function Home() {
-  redirect("/home");
+export default async function Home() {
+  return (
+    <div>
+      <FirstLoading />
+      {await new Promise((resolve) => setTimeout(resolve, 2000))}
+      {redirect("/home")}
+    </div>
+  );
 }
---
+++ components/template/loading/firstLoading.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function FirstLoading() {
+  return (
+    <div>
+      <h2 className="text-8xl">در حال انقال شما به سایت هستیم</h2>
+    </div>
+  );
+}
