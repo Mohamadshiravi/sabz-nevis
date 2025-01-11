2be4d75f77fb082d88f2eb6b2326ed7677226c9e
mohamad <mohamadshiravi85@gmail.com>
2025-01-11 14:55:31 +0330

    added firstLoading

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
 import FirstLoading from "@/components/template/loading/firstLoading";
-import { redirect } from "next/navigation";
 
 export default async function Home() {
-  return (
-    <div>
-      <FirstLoading />
-      {await new Promise((resolve) => setTimeout(resolve, 2000))}
-      {redirect("/home")}
-    </div>
-  );
+  return <FirstLoading />;
 }
--- components/template/loading/firstLoading.tsx
+++ components/template/loading/firstLoading.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function FirstLoading() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/home");
+  }, []);
   return (
-    <div>
-      <h2 className="text-8xl">در حال انقال شما به سایت هستیم</h2>
-    </div>
+    <section className="w-full h-screen fixed top-0 left-0 bg-white flex items-center justify-center text-zinc-800 text-3xl">
+      <h2>در حال بررسی مرورگر، پیش از انتقال به سایت هستیم ...</h2>
+    </section>
   );
 }
