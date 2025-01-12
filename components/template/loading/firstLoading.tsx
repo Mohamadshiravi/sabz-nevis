58d4ef9c5665d9dc80a2ed79fa74ab621a5b99fb
mohamad <mohamadshiravi85@gmail.com>
2025-01-12 17:15:23 +0330

    finding deploy bug

--- components/template/loading/firstLoading.tsx
+++ components/template/loading/firstLoading.tsx
@@ -11,7 +11,7 @@ export default function FirstLoading() {
   return (
     <section className="w-full h-screen fixed top-0 left-0 bg-white flex items-center justify-center text-zinc-800 sm:text-3xl text-2xl">
       <h2 className="text-center">
-        در حال بررسی مرورگر، پیش از انتقال به سایت هستیم ...
+        در حال بررسی مرورگر، پیش از انتقال به سایت هستیم ......
       </h2>
     </section>
   );
