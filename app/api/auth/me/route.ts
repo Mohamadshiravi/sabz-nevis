c4889a57b6b21d11497f9bf963f7de946d662504
mohamad <mohamadshiravi85@gmail.com>
2025-01-12 17:24:11 +0330

    deploy bug fixed

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -3,6 +3,8 @@ import userModel from "@/models/user";
 import { VerifyAccessToken } from "@/utils/auth/tokenControl";
 import { cookies } from "next/headers";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req: Request) {
   try {
     const token = cookies().get("token")?.value;
