9d16f811e771b1379e1be9f4e108e8460180dde7
mohamad <mohamadshiravi85@gmail.com>
2025-02-06 19:29:25 +0330

    fix UI bug

--- components/template/me/posts/postsTitle.tsx
+++ components/template/me/posts/postsTitle.tsx
@@ -7,8 +7,9 @@ export default function PostsTitle() {
   const router = useRouter();
   return (
     <div className="flex items-center justify-between mt-20">
-      <h1 className="vazir-bold text-4xl">پست های شما</h1>
+      <h1 className="vazir-bold sm:text-4xl text-3xl">پست های شما</h1>
       <LoadingBtn
+        width="sm:w-[200px] w-[130px]"
         onPress={() => {
           localStorage.removeItem("postId");
           router.push("/post/create");
