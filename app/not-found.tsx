3d086ee28b481fee93c186b83765413f0086d9b2
mohamad <mohamadshiravi85@gmail.com>
2025-02-08 16:33:32 +0330

    add notFound page

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-center">
+      <h1 className="text-9xl font-bold text-myGreen-600">404</h1>
+      <p className="my-4 text-xl text-myText-600 vazir-bold">
+        صفحه موردنظر شما در سبز نویس پیدا نشد!
+      </p>
+      <span
+        className={`text-sm w-[200px] cursor-pointer vazir-medium flex items-center justify-center gap-2 hover:bg-zinc-700 hover:text-white transition px-4 h-[35px] border-2 border-zinc-700 text-zinc-700 dark:border-zinc-300 dark:text-zinc-300 dark:hover:bg-zinc-300 dark:hover:text-zinc-800 rounded-full`}
+      >
+        بازگشت به صفحه اصلی
+      </span>
+    </div>
+  );
+}
